2233e2988a6bd62bfb9bb107a49a6e42f8571c5b
ansht2000 <ansht2000@gmail.com>
2024-09-03 17:41:46 -0700

    added function to shoot bullets from the player sprite

--- circleshape.py
+++ circleshape.py
@@ -20,6 +20,6 @@ class CircleShape(pygame.sprite.Sprite):
         pass
 
     def collision_detected(self, other):
-        return self.position.distance_to(other.position) < self.radius + other.radius
+        return self.position.distance_to(other.position) <= self.radius + other.radius
         
     
